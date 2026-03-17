638baf37c95b8e3470503e2b563f56f1933f2390
Gustavo de Farias <zacharingustavo@gmail.com>
2026-03-17 19:44:29 -0300

    update

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from processamento import (
-    validar_notas,
-)
-
 def alunos():
     dados = [
         ("Joseph", [9, 9.5, 10, 5.8, 4.6, 3.91]),
@@ -10,14 +6,3 @@ def alunos():
         ("Tony", [7, 6.5, 7.7, 5.8, 4, 10]),
         ("Cristiano", [10, 9.5, 3, 5, 7.78]),
     ]
-
-    alunos_validos = []
-
-    for nome, notas in dados:
-        if validar_notas(notas):
-            alunos_validos.append((nome))
-        else:
-            print(f"Dados inválidos para {nome}")
-
-
-alunos()
