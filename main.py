02f8833661f01f14e3c2c03e4daabb34ac7ba9f9
Gustavo de Farias <zacharingustavo@gmail.com>
2026-03-17 19:47:12 -0300

    Tratamento de Dados finished

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from processamento import(
+    validar_notas,
+)
+
 def alunos():
     dados = [
         ("Joseph", [9, 9.5, 10, 5.8, 4.6, 3.91]),
@@ -6,3 +10,11 @@ def alunos():
         ("Tony", [7, 6.5, 7.7, 5.8, 4, 10]),
         ("Cristiano", [10, 9.5, 3, 5, 7.78]),
     ]
+
+    alunos_validos = []
+
+    for nome, notas in dados:
+        if validar_notas(notas):
+            alunos_validos.append((nome))
+        else:
+            print(f"Dados inválidos para {nome}")
