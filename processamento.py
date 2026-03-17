bfe9e7f1a8ee218e6e8f8805b5d29fe559fd871f
Gustavo de Farias <zacharingustavo@gmail.com>
2026-03-17 19:58:07 -0300

    calcula media function add

--- processamento.py
+++ processamento.py
@@ -1,2 +1,8 @@
 def validar_notas(notas):
     return isinstance(notas, list) and len(notas) > 0
+
+def calcular_media(notas):
+    soma = 0
+    for nota in notas:
+        soma += nota
+    return soma / len(notas)
